9ed397c32ad39a86da62cbde7db8d1f628cf4261
josh10.moonshot <robalausej10@gmail.com>
2026-04-01 23:22:05 +0800

    fix: replace Asia/Manila timezone with UTC for Render PostgreSQL compatibility

--- backend/config/database.go
+++ backend/config/database.go
@@ -15,7 +15,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Manila",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
 		GetEnv("DB_HOST", "localhost"),
 		GetEnv("DB_USER", "postgres"),
 		GetEnv("DB_PASSWORD", "postgres"),
